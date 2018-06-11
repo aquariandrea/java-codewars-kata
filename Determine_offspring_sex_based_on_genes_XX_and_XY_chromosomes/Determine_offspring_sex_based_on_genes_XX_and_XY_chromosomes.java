375593a0653a424b0eb87952327b27a2f69a050b
Andrea Aquari <aquariandrea@gmail.com>
2018-06-11 13:51:08 +0200

    [Codewars.com] - Complete Determine offspring sex based on genes XX and XY chromosomes

---
+++ Determine_offspring_sex_based_on_genes_XX_and_XY_chromosomes/Determine_offspring_sex_based_on_genes_XX_and_XY_chromosomes.java
@@ -0,0 +1,5 @@
+public class Kata {
+  public static String chromosomeCheck(String sperm) {
+    return sperm == "XX" ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son.";
+  }
+}
