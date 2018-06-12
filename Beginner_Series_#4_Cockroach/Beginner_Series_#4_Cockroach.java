238167d91151b74437fdc1bdf5616cc1371d9e01
Andrea Aquari <aquariandrea@gmail.com>
2018-06-12 13:10:03 +0200

    [Codewars.com] - Complete Beginner Series #4 Cockroach

---
+++ Beginner_Series_#4_Cockroach/Beginner_Series_#4_Cockroach.java
@@ -0,0 +1,5 @@
+public class Cockroach{
+  public int cockroachSpeed(double x){
+    return (int) (x * 10000) / 360;
+  }
+}
--- Moves_in_squared_strings_(I)/Moves_in_squared_strings_(I).java
+++ Moves_in_squared_strings_(I)/Moves_in_squared_strings_(I).java
@@ -1,4 +1,5 @@
 import java.util.function.UnaryOperator;
+
 class Opstrings {
 
     public static String vertMirror (String strng) {
