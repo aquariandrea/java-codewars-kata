02ceae116917bad5a28d076f5265e7a8d9655f08
Andrea Aquari <aquariandrea@gmail.com>
2018-06-12 15:08:04 +0200

    [Codewars.com] - Complete Welcome to the City

---
+++ Welcome_to_the_City/Welcome_to_the_City.java
@@ -0,0 +1,12 @@
+public class Hello{
+  public String sayHello(String [] name, String city, String state){
+    return "Hello, "
+          + String.join(" ", name)
+          + "! "
+          + "Welcome to "
+          + city
+          + ", "
+          + state
+          + "!";
+  }
+}
