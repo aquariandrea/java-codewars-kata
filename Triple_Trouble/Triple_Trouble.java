b6750b3a4c4bdd576e0775c435b96804fc5c522e
Andrea Aquari <aquariandrea@gmail.com>
2018-06-12 18:44:35 +0200

    [Codewars.com] - Complete Triple Trouble

---
+++ Triple_Trouble/Triple_Trouble.java
@@ -0,0 +1,9 @@
+public class Kata {
+  public static String tripleTrouble(String one, String two, String three) {
+    StringBuilder myBuilder = new StringBuilder();
+    for(int i = 0; i < one.length(); i++){
+      myBuilder.append(String.valueOf(one.charAt(i)) + String.valueOf(two.charAt(i)) + String.valueOf(three.charAt(i)));
+    }
+    return myBuilder.toString();
+  }
+}
