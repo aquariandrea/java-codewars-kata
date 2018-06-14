66106ed51e456955503686cab1f1a97c7d6400e5
Andrea Aquari <aquariandrea@gmail.com>
2018-06-14 12:38:24 +0200

    [Codewars.com] - Complete Leonardo Dicaprio and Oscars

---
+++ Leonardo_Dicaprio_and_Oscars/Leonardo_Dicaprio_and_Oscars.java
@@ -0,0 +1,6 @@
+public class Kata
+{
+  public static String leo(final int oscar){
+    return (oscar > 88) ? "Leo got one already!" : (oscar == 88) ? "Leo finally won the oscar! Leo is happy" : (oscar == 87 || oscar < 86) ? "When will you give Leo an Oscar?" : "Not even for Wolf of wallstreet?!";
+  }
+}
