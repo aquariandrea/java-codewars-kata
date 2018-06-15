6a08fc899c0a2bed71b90f4e2c97350584952765
Andrea Aquari <aquariandrea@gmail.com>
2018-06-15 13:19:28 +0200

    [Codewars.com] -Circles_in_Polygons -I_love_you,_a_little_,_a_lot,_passionately...not_at_all

---
+++ Circles_in_Polygons/Circles_in_Polygons.java
@@ -0,0 +1,15 @@
+public class Polygon {
+    int sides;
+    int sideLength;
+
+    public Polygon(int sides, int sideLength) {
+        this.sides = sides;
+        this.sideLength = sideLength;
+    }
+
+    public double circleDiameter(){
+        double angle_in_radian =  Math.PI / sides;
+        return sideLength / Math.tan(angle_in_radian);
+    }
+
+}
