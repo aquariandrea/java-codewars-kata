6b2d4ef69b08c8abe1f643d8ab34c763634588ea
Andrea Aquari <aquariandrea@gmail.com>
2018-11-05 19:58:13 +0100

    [Codewars.com] Complete Two to One

---
+++ Two_to_One/Two_to_One.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class TwoToOne {
+    public static String longest (String s1, String s2) {
+        String myString = s1 + s2;
+        return Arrays.asList(myString.split("")).stream().sorted().distinct().collect(Collectors.joining());
+    }
+}
